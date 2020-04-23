dd8730351c24bd38b57f85a52c2bff04c822e463
Erick Zanardo <erickzanardoo@gmail.com>
2020-04-23 20:54:53 -0300

    removing example runner project and adding it to gitignore

--- example/ios/Flutter/flutter_export_environment.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-# This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/erick/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/erick/projects/flutter_gapless_audio_loop/example"
-export "FLUTTER_TARGET=/Users/erick/projects/flutter_gapless_audio_loop/example/lib/main.dart"
-export "FLUTTER_BUILD_DIR=build"
-export "SYMROOT=${SOURCE_ROOT}/../build/ios"
-export "FLUTTER_FRAMEWORK_DIR=/Users/erick/flutter/bin/cache/artifacts/engine/ios"
